3304ce71fb1750bd8eecc8235cc1044e734fc9ed
Brian Button <bbutton@agilestl.com>
2012-12-13 18:30:11 -0600

    random whitespace change

--- fizzBuzz.hs
+++ fizzBuzz.hs
@@ -1,8 +1,7 @@
-fizzBuzz :: Int -> String
 fizzBuzz x
 	| x `mod` 15  == 0 = "FizzBuzz!!!"
 	| x `mod` 3  == 0 = "Fizz"
 	| x `mod` 5  == 0 = "Buzz"
-	| otherwise = show x
+	| otherwise = show x 
 
 
