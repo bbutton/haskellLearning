590effda5bb96c3f8664b29353ac3420cccdd8c0
Brian Button <bbutton@agilestl.com>
2013-02-02 06:15:55 -0600

    Working version

--- ch4/take2.hs
+++ ch4/take2.hs
@@ -7,9 +7,10 @@ getColumn ((r:row):rs) = r:(getColumn rs)
 stripColumn :: [[a]] -> [[a]]
 stripColumn [] = []
 stripColumn ([]:rs) = []
-stripColumn (row:[]) = (tail row):stripColumn []
+stripColumn (row:[]) = (tail row):[]
 stripColumn (row:rs) = (tail row):stripColumn rs
 
 transpose :: [[a]] -> [[a]]
 transpose [] = []
+transpose ([]:rs) = []
 transpose rows = (getColumn rows) : (transpose $ stripColumn rows)
