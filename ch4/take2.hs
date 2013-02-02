534da434a9d7519e2031bd6594b3dbcd3d0416e1
Brian Button <bbutton@agilestl.com>
2013-02-01 19:38:42 -0600

    working, but going through stripColumn one too many times

--- ch4/take2.hs
+++ ch4/take2.hs
@@ -1,17 +1,15 @@
 getColumn :: [[a]] -> [a]
---getColumn ((r:[]):[]) = error "help"
 getColumn [] = []
---getColumn ([]:[]) = []
+getColumn ([]:rows) = []
 getColumn ((r:row):[]) = r:(getColumn [])
 getColumn ((r:row):rs) = r:(getColumn rs)
 
 stripColumn :: [[a]] -> [[a]]
 stripColumn [] = []
+stripColumn ([]:rs) = []
 stripColumn (row:[]) = (tail row):stripColumn []
 stripColumn (row:rs) = (tail row):stripColumn rs
 
 transpose :: [[a]] -> [[a]]
 transpose [] = []
 transpose rows = (getColumn rows) : (transpose $ stripColumn rows)
-
--- transpose (row:[]) = getColumn (row:[]):transpose (tail row):[]
